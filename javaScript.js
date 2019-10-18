35951ef4c52af3cdcb82c40928425571b34f0aca
changuitoss <ezealvarezrubio@gmail.com>
2019-10-18 12:02:17 -0300

    Probando el playsRock()

--- javaScript.js
+++ javaScript.js
@@ -1,27 +1,64 @@
 const posibilidades = ['rock', 'paper', 'scissors'];
 
+var computerSelection;
+var playerSelection;
+
+var puntajePlayer = 0;
+var puntajeAi = 0;
+var roundsTotal = 0;
+
 
 const computerPlay = () => {
 	var azar = Math.floor(Math.random() * 3)
 	return posibilidades[azar];
 }
 
-var computerSelection = computerPlay();
-const playerSelection = 'rock';
+/*const playsRock = () => {
+		console.log('ejecutando playsRock');
+		if (playerSelection == 'rock') {
+			if (computerSelection == 'scissors') {
+				console.log('You win! Rock beats scissors');
+				return puntajePlayer += 1;
+			} else if(computerSelection == 'paper') {
+				console.log('You lose! Paper beats Rock');
+				return puntajeAi =+ 1;
+			} else {
+				console.log('It´s a tie!');
+				return 'It´s a tie!';
+			}
+		}
+}*/
+
 
 const round = (playerSelection, computerSelection) => {
-	if(playerSelection == 'rock') {
+	playerSelection = prompt('choose your weapon').toLowerCase();
+	var computerSelection = computerPlay();
+	console.log('playerSelection: ', playerSelection);
+	console.log('computerSelection: ', computerSelection);
+	if (playerSelection == 'rock') {
 		if (computerSelection == 'scissors') {
 			console.log('You win! Rock beats scissors');
-			return 'You win! Rock beats scissors';
+			return puntajePlayer += 1;
 		} else if(computerSelection == 'paper') {
 			console.log('You lose! Paper beats Rock');
-			return 'You win! Paper beats Rock';
+			return puntajeAi =+ 1;
 		} else {
 			console.log('It´s a tie!');
 			return 'It´s a tie!';
 		}
 	}
 }
+//playsRock();
 
-round(playerSelection, computerSelection);
+
+const game = () => {
+	for(var i = 0; i < 6; i++) {
+		roundsTotal += 1;
+		round(playerSelection, computerSelection);
+		console.log('roundsTotal: ', roundsTotal);
+		console.log('puntajePlayer:', puntajePlayer)
+		console.log('puntajeAi: ', puntajeAi)
+	}
+}
+
+game();
